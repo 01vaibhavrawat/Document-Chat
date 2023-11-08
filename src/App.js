6d6a372f70fb701d0a3801fa7bc6e9bda9487904
unknown <vaibhrawat@gmail.com>
2023-11-08 13:09:52 +0530

    set up of basic structure of sidebar and navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Layout from "./containers/layout/Layout";
+
+function App() {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/customTheme.js
@@ -0,0 +1,7 @@
+const customTheme = {
+    primary_background : "#2d2d2a",
+    secondary_background: "#4c4c47",
+    primary_text: "#e5dcc5"
+}
+
+export default customTheme;
